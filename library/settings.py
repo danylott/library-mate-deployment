90f20f47415765e174205cfce26622b35c7be8af
danylott <d.tuytuyshkin@gmail.com>
2022-04-19 19:00:13 +0300

    library-mate.herokuapp.com added to Allowed hosts

--- library/settings.py
+++ library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-i6l^3td*)xvk-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "library-mate.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
