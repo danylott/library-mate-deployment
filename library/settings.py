a2594a36aa1bcb6408334cf6a26c217ddcba5ed2
danylott <d.tuytuyshkin@gmail.com>
2022-04-19 19:14:57 +0300

    finalizing app deployment

--- library/settings.py
+++ library/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-i6l^3td*)xvk-3hm#ue)!$7tunl1d)c_-a=tz1^y!0=fi@k0e@")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
