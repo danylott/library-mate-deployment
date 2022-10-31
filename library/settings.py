428f832afd4e7fcaa3734f09f18d90fb9c155e97
danylott <d.tuytuyshkin@gmail.com>
2022-10-31 15:34:41 +0200

    remove https from ALLOWED_HOSTS

--- library/settings.py
+++ library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-i6l^3td*)xvk-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://danylott.pythonanywhere.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "danylott.pythonanywhere.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
